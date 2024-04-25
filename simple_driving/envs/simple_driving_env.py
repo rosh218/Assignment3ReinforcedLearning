41db832ca19a0b5949097f07d5c0a178f770e615
rosh218 <rosh.m.delacruz@student.uts.edu.au>
2024-04-25 17:25:27 +1000

    changes

--- simple_driving/envs/simple_driving_env.py
+++ simple_driving/envs/simple_driving_env.py
@@ -8,6 +8,7 @@ from simple_driving.resources.plane import Plane
 from simple_driving.resources.goal import Goal
 import matplotlib.pyplot as plt
 import time
+from collections import defaultdict
 
 RENDER_HEIGHT = 720
 RENDER_WIDTH = 960
@@ -46,46 +47,44 @@ class SimpleDrivingEnv(gym.Env):
         self.render_rot_matrix = None
         self.reset()
         self._envStepCounter = 0
+        
 
-    def step(self, action):
+    def step(self, epsilon, Q):
         # Feed action to the car and get observation of car's state
         if (self._isDiscrete):
-            fwd = [-1, -1, -1, 0, 0, 0, 1, 1, 1]
-            steerings = [-0.6, 0, 0.6, -0.6, 0, 0.6, -0.6, 0, 0.6]
-            throttle = fwd[action]
-            steering_angle = steerings[action]
-            action = [throttle, steering_angle]
+            action = SimpleDrivingEnv.epsilon_greedy(self, self.car.get_observation(), Q, epsilon, 1, 0)
+        else:
+            # Implement continuous action space here
+            pass
         self.car.apply_action(action)
         for i in range(self._actionRepeat):
-          self._p.stepSimulation()
-          if self._renders:
-            time.sleep(self._timeStep)
+            self._p.stepSimulation()
+            if self._renders:
+                time.sleep(self._timeStep)
 
-          carpos, carorn = self._p.getBasePositionAndOrientation(self.car.car)
-          goalpos, goalorn = self._p.getBasePositionAndOrientation(self.goal_object.goal)
-          car_ob = self.getExtendedObservation()
+            carpos, carorn = self._p.getBasePositionAndOrientation(self.car.car)
+            goalpos, goalorn = self._p.getBasePositionAndOrientation(self.goal_object.goal)
+            car_ob = self.getExtendedObservation()
 
-          if self._termination():
-            self.done = True
-            break
-          self._envStepCounter += 1
+            if self._termination():
+                self.done = True
+                break
+            self._envStepCounter += 1
 
-        # Compute reward as L2 change in distance to goal
-        # dist_to_goal = math.sqrt(((car_ob[0] - self.goal[0]) ** 2 +
-                                  # (car_ob[1] - self.goal[1]) ** 2))
-        dist_to_goal = math.sqrt(((carpos[0] - goalpos[0]) ** 2 +
-                                  (carpos[1] - goalpos[1]) ** 2))
-        # reward = max(self.prev_dist_to_goal - dist_to_goal, 0)
-        reward = -dist_to_goal
-        self.prev_dist_to_goal = dist_to_goal
+            # Compute reward as L2 change in distance to goal
+            dist_to_goal = math.sqrt(((carpos[0] - goalpos[0]) ** 2 +
+                                    (carpos[1] - goalpos[1]) ** 2))
+            reward = -dist_to_goal
+            self.prev_dist_to_goal = dist_to_goal
 
-        # Done by reaching goal
-        if dist_to_goal < 1.5 and not self.reached_goal:
-            #print("reached goal")
-            self.done = True
-            self.reached_goal = True
+            # Done by reaching goal
+            if dist_to_goal < 1.5 and not self.reached_goal:
+                #print("reached goal")
+                self.done = True
+                self.reached_goal = True
 
-        ob = car_ob
+            ob = car_ob
+
         return ob, reward, self.done, dict()
 
     def seed(self, seed=None):
@@ -191,3 +190,94 @@ class SimpleDrivingEnv(gym.Env):
 
     def close(self):
         self._p.disconnect()
+
+
+    def epsilon_greedy(env, state, Q, epsilon, episodes, episode):
+        """Selects an action to take based on a uniformly random sampled number.
+        If this number is greater than epsilon then returns action with the largest
+        Q-value at the current state. Otherwise it returns a random action.
+
+        Args:
+            env: gym object.
+            state: current state
+            Q: Q-function. This is a dictionary that is indexed by the state and
+            returns an array of Q-values for each action at that state. For example,
+            Q[0] will return an array of Q-values for state 0 where the index of
+            the array corresponds to the action.
+            epsilon: control how often you explore random actions versus focusing on
+                    high value state and actions
+            episodes: maximum number of episodes (used in other epsilon greedy variant later)
+            episode: number of episodes played so far (used in other epsilon greedy variant later)
+
+        Returns:
+            Action to be executed for next step.
+        """
+        if np.random.uniform(0, 1) > epsilon:
+            #### return the action with the highest Q value at the given state  ####
+            return np.argmax(Q[state])
+            ########################################################################
+        else:
+            return env.action_space.sample()
+        
+
+    def simulate(env, Q, max_episode_length, epsilon, episodes, episode):
+        """Rolls out an episode of actions to be used for learning.
+
+        Args:
+            env: gym object.
+            Q: state-action value function
+            epsilon: control how often you explore random actions versus focusing on
+                    high value state and actions
+            episodes: maximum number of episodes
+            episode: number of episodes played so far
+
+        Returns:
+            Dataset of episodes for training the RL agent containing states, actions and rewards.
+        """
+        D = []
+        state = env.reset()                                                     # line 2 - note we don't sample the start state since this is predefined
+        done = False
+        for step in range(max_episode_length):                                  # line 3
+            action = SimpleDrivingEnv.epsilon_greedy(env, state, Q, epsilon, episodes, episode)  # line 4
+            next_state, reward, done, info = env.step(action)                   # line 5
+            #### change reward so that a negative reward is given if the agent falls down a hole #######
+            if env.desc[int(next_state/env.ncol), int(next_state % env.ncol)] == b'H':  # fell in hole
+                reward = -1.0
+            ############################################################################################
+            D.append([state, action, reward, next_state])                       # line 7
+            state = next_state                                                  # line 8
+            if done:                                                            # if we fall into a hole or reach treasure then end episode
+                break
+        return D                                                                # line 10
+
+    def q_learning(env, gamma, episodes, max_episode_length, epsilon, step_size):
+        """Main loop of Q-learning algorithm.
+
+        Args:
+            env: gym object.
+            gamma: discount factor - determines how much to value future actions
+            episodes: number of episodes to play out
+            max_episode_length: maximum number of steps for episode roll out
+            epsilon: control how often you explore random actions versus focusing on
+                    high value state and actions
+            step_size: learning rate - controls how fast or slow to update Q-values
+                    for each iteration.
+
+        Returns:
+            Q-function which is used to derive policy.
+        """
+        Q = defaultdict(lambda: np.zeros(env.action_space.n))                       # line 2
+        total_reward = 0
+        for episode in range(episodes):                                             # slightly different to line 3, we just run until maximum episodes played out
+            D = SimpleDrivingEnv.simulate(env, Q, max_episode_length, epsilon, episodes, episode)    # line 4
+            for data in D:                                                          # data = [state, action, reward, next_state]  (line 5)
+                # print(data)
+                ####################### update Q value (line 6) #########################
+                Q[data[0]][data[1]] = (1 - step_size) * Q[data[0]][data[1]] + step_size * (data[2] +  gamma * np.max(Q[data[3]]))  # line 6
+                #########################################################################
+                total_reward += data[2]
+                # input()
+            if episode % 100 == 0:
+                print("average total reward per episode batch since episode ", episode, ": ", total_reward/ float(100))
+                total_reward = 0
+        return Q  # line 9
